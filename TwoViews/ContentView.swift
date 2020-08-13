cf5fc88955c64f8c49efcd132bd3fc71caaeb479
nakaotatsuya <earthdefense10@gmail.com>
2020-08-13 20:40:35 +0900

    Initial Commit

---
+++ TwoViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwoViews
+//
+//  Created by 中尾　達也 on 2020/08/13.
+//  Copyright © 2020 Sample Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
